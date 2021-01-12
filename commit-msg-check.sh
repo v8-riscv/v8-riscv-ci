191058341a71bded17e5baeae56eac31d625ea1f
Brice Dobry <brice.dobry@futurewei.com>
2021-01-12 15:10:39 -0500

    Add context for message checks

--- commit-msg-check.sh
+++ commit-msg-check.sh
@@ -11,30 +11,35 @@ for commit in $commits; do
         # Next, check the requirements of the line
         if [[ $lineNum -eq 0 ]]; then
             if [[ ${#line} -gt 50 ]]; then
+                echo "$commit: $line"
                 echo "Commit message: Limit the subject line to 50 characters"
                 echo "https://chris.beams.io/posts/git-commit/#limit-50"
                 exit 1
             fi
 
             if [[ $line =~ ^[[:lower:]] ]]; then
+                echo "$commit: $line"
                 echo "Commit message: Capitalize the subject line"
                 echo "https://chris.beams.io/posts/git-commit/#capitalize"
                 exit 1
             fi
 
             if [[ $line =~ \.$ ]]; then
+                echo "$commit: $line"
                 echo "Commit message: Do not end the subject line with a period"
                 echo "https://chris.beams.io/posts/git-commit/#end"
                 exit 1
             fi
         elif [[ $lineNum -eq 1 ]]; then
             if [[ ${#line} -ne 0 ]]; then
+                echo "$commit: $line"
                 echo "Commit message: Separate subject from body with a blank line"
                 echo "https://chris.beams.io/posts/git-commit/#separate"
                 exit 1
             fi
         else
             if [[ ${#line} -gt 72 ]]; then
+                echo "$commit: $line"
                 echo "Commit message: Wrap the body at 72 characters"
                 echo "https://chris.beams.io/posts/git-commit/#wrap-72"
                 exit 1
